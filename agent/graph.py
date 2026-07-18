161716ad89783c3679b4d80923c54dcf1cd81bd3
iamthisal <thisaldilnath@gmail.com>
2026-07-18 18:43:10 +0530

    planner working with structured output, refactored into prompts/states/graph modules

--- agent/graph.py
+++ agent/graph.py
@@ -1,12 +1,27 @@
 from langchain_groq import  ChatGroq
 from dotenv import load_dotenv
 
+from prompts import *
+from state import *
+
 load_dotenv()
 
 
-llm = ChatGroq(model="openai/gpt-oss-120b")
+llm = ChatGroq(model="llama-3.3-70b-versatile")
 
 
-response = llm.invoke("who invented yoga")
 
-print(response.content)
+
+user_prompt   = "Create a simple calculator web application"
+
+
+
+prompt = prompt(user_prompt)
+
+
+
+response = llm.with_structured_output(Plan).invoke(prompt)
+
+print(response.model_dump())
+
+
---
+++ agent/prompts.py
@@ -0,0 +1,8 @@
+from graph import user_prompt
+
+
+prompt = f"""
+you are the PLANNER agent. Convert the user prompt into a COMPLETE engineering project plan
+
+User request: {user_prompt}
+"""
