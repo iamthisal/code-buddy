a5de23e96b16bb4aff9e626e76efba23251a716a
iamthisal <thisaldilnath@gmail.com>
2026-07-19 16:09:13 +0530

    architect node working, returns task plan with implementation steps

--- agent/graph.py
+++ agent/graph.py
@@ -1,8 +1,12 @@
 from langchain_groq import  ChatGroq
 from dotenv import load_dotenv
-
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from prompts import *
 from state import *
+from langgraph.constants import END
+from langgraph.graph import StateGraph
 
 load_dotenv()
 
@@ -12,16 +16,45 @@ llm = ChatGroq(model="llama-3.3-70b-versatile")
 
 
 
-user_prompt   = "Create a simple calculator web application"
 
 
 
-prompt = prompt(user_prompt)
 
 
 
-response = llm.with_structured_output(Plan).invoke(prompt)
+def planner_agent(state: dict) -> dict:
+    user_prompt = state["user_prompt"]
+    response = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    if response is None:
+        raise ValueError("planner did not return a valid response")
+    return {"plan" : response}
 
-print(response.model_dump())
 
+def architect_agent(state:dict) -> dict:
+    plan : Plan = state["plan"]
+    response = llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if response is None:
+        raise ValueError("Architect did not return a valid response")
+   #adding the plan attribute for the state(Taskplan)
+    response.plan = plan #without model config this would break
+
+    return {"task_plan" : response}
+
+
+
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.add_node("architect",architect_agent)
+graph.add_edge("planner","architect")
+graph.set_entry_point("planner")
+
+
+
+
+agent = graph.compile()
+
+user_prompt = "create a simple calculator web application"
+
+result = agent.invoke({"user_prompt": user_prompt})
+print(result)
 
--- agent/state.py
+++ agent/state.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel , Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import List
 
 
@@ -19,3 +19,18 @@ class Plan(BaseModel):
         features: list[str] = Field(
             description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.")
         files: list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
+
+class ImplementationTask(BaseModel):
+    filepath: str = Field(description="The path to the file to be modified")
+    task_description: str = Field(description="A description of the task to be executed")
+
+
+class TaskPlan(BaseModel):
+    implementation_steps : list[ImplementationTask] = Field(description="A list of steps to be executed")
+    model_config = ConfigDict(extra="allow")  #support to add new attributes
+
+
+
+
+
+
